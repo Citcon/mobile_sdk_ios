d362772c726fbf15cb8d615122324c406183a813
yansheng.ao <yansheng.ao@citcon.cn>
2024-12-02 14:58:23 +0800

    Update test code

--- CPay.podspec
+++ CPay.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CPay'
-  s.version          = '2.7.2'
+  s.version          = '2.8.0'
   s.summary          = 'Online mobile SDK for iOS.'
 
 # This description is used to generate tags and improve search results.
--- CPayDemo/CPayDemo/ViewController.m
+++ CPayDemo/CPayDemo/ViewController.m
@@ -56,6 +56,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *noteField;
 @property (weak, nonatomic) IBOutlet UISwitch *autoCaptureSwitcher;
 @property (weak, nonatomic) IBOutlet UIButton *chargeBtn;
+@property (weak, nonatomic) IBOutlet UISwitch *requestTokenSwitcher;
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
@@ -74,6 +75,10 @@
     NSLog(@"SDK Version: %@", [CPayManager getVersion]);
 
     NSLog(@"yunshanfu install: %d",[CPayManager isUnionPayInstalled]);
+    NSLog(@"weixin install: %d",[CPayManager isWechatInstalled]);
+    NSLog(@"alipay install: %d",[CPayManager isAlipayInstalled]);
+    NSLog(@"alipay hk install: %d",[CPayManager isAlipayHKInstalled]);
+
 }
 
 - (void)initUI {
@@ -205,6 +210,8 @@
         order.taxableAmount = _txtTaxableAmount.text;
         order.taxExemptAmount = _txtTaxExemptAmount.text;
         order.totalTaxAmount = _txtTotalTaxAmount.text;
+    } else {
+        order.requestToken = _requestTokenSwitcher.isOn;
     }
     
     
